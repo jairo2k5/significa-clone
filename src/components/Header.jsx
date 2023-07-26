cffc2cba75f2b9c32e0f6fd222bc8f65d08d693a
Jairo Holanda da Silva <jairoholanda007@gmail.com>
2023-07-26 10:29:01 -0300

    primeira versão da header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,23 @@
+import { Link } from 'react-router-dom'
+import Menu from './Menu'
+import './styles/Header.sass'
+
 function Header() {
     return (
-        <>
-            Header
-        </>
+        <div className="header">
+            <Link to={'/'}>
+                <img src="/significa.svg" alt="Significa" />
+            </Link>
+            <div className="links">
+                <Link to={'/projects'}>Projects</Link>
+                <Link to={'/services'}>Services</Link>
+                <Link to={'/about'}>About</Link>
+                <Link to={'/get-a-quote'}>
+                    <button>Get a quote</button>
+                </Link>
+                <Menu />
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import './styles/Menu.sass'
+
+function Menu() {
+    return (
+        <div className="menu">
+            ...
+        </div>
+    )
+}
+
+export default Menu
