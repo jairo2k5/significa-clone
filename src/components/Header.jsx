132deef5ce804fcfab98d6be6ee1a934fb38c3a4
Jairo Holanda da Silva <jairoholanda007@gmail.com>
2023-07-27 14:34:07 -0300

    rotas da header transformadas em componentes; responsividade aprimorada; sidebar bug fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Sidebar = styled.div`
     display: ${props => props.active ? 'flex' : 'none'};
     flex-direction: column;
     padding: 20px;
-    position: absolute;
+    position: fixed;
     top: 0;
     right: 0;
     height: 100%;
@@ -24,6 +24,12 @@ const Sidebar = styled.div`
         display: flex;
         justify-content: space-between;
         align-items: center;
+    }
+    .top-sidebar {
+        .right {
+            display: flex;
+            flex-direction: row;
+        }
     }
 `
 
@@ -44,6 +50,34 @@ const Menu = styled.button`
     }
 `
 
+const GetQuote = styled(Link)`
+    text-decoration: none;
+    background: #f1f1f1;
+    color: #222;
+    padding: 10px 20px 10px 20px;
+    border: none;
+    cursor: pointer;
+    border-radius: 15px;
+    transition-duration: 150ms;
+    font-weight: 600;
+    font-size: 1em;
+    &:hover {
+        box-shadow: 0px 0px 2px 2px #ffb44396;
+    }
+`
+
+const Routes = styled(Link)`
+    text-decoration: none;
+    color: #fff;
+    margin: 0px 10px 0px 10px;
+    font-size: 1em;
+    border-bottom: 2px solid rgba(0, 0, 0, 0);
+    transition: 250ms;
+    &:hover {
+        border-bottom: 2px solid #ddd;
+    }
+`
+
 function Header() {
     const [isActive, setIsActive] = useState(false)
 
@@ -55,21 +89,24 @@ function Header() {
                 </Link>
             </div>
             <div className="links">
-                <Link to={'/projects'} is_route='true'>Projects</Link>
-                <Link to={'/services'} is_route='true'>Services</Link>
-                <Link to={'/about'} is_route='true'>About</Link>
-                <Link to={'/get-a-quote'}>
-                    <button>Get a quote</button>
-                </Link>
+                <div className="header-routes">
+                    <Routes to={'/projects'}>Projects</Routes>
+                    <Routes to={'/services'}>Services</Routes>
+                    <Routes to={'/about'}>About</Routes>
+                </div>
+                <GetQuote to={'/get-a-quote'} className='links-gq'>Get a quote</GetQuote>
                 <Menu onClick={() => { setIsActive(!isActive) }}>
                     <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M3 7h2v2H3zM7 7h2v2H7zM11 7h2v2h-2z"></path><path d="M8.6 8.8c-.5 0-1.2.1-1.4-.4 0-.2-.2-.7.1-.7.2 0 .2-.3.5-.3.1 0 .5 0 .6.2M11.7 8.6l-.3-.3V8c0-.3.1-.3.4-.4.2-.1.5-.2.7 0l.2.4v.5M4.3 8.6c.2 0 .3-.2.4-.3V8c0-.3-.2-.3-.4-.4-.2-.1-.6-.2-.8 0l-.1.4v.5" stroke="#fff" stroke-width="1.2" stroke-linecap="round"></path></svg>
                 </Menu>
                 <Sidebar active={isActive}>
                     <div className="top-sidebar">
                         <svg width="24" height="29" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 27.887c-6.39 0-11.59-5.04-11.59-11.285C.41 9.563 6.005.614 12 .614c5.995 0 11.591 8.948 11.591 15.988 0 6.245-5.2 11.285-11.59 11.285Zm0-2.573c5.006 0 9.044-3.913 9.044-8.712 0-5.797-4.765-13.415-9.044-13.415-4.279 0-9.043 7.618-9.043 13.415 0 4.8 4.037 8.712 9.043 8.712Z" fill="#fff"></path></svg>
-                        <Menu onClick={() => { setIsActive(!isActive) }}>
-                            <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M3 7h2v2H3zM7 7h2v2H7zM11 7h2v2h-2z"></path><path d="M8.6 8.8c-.5 0-1.2.1-1.4-.4 0-.2-.2-.7.1-.7.2 0 .2-.3.5-.3.1 0 .5 0 .6.2M11.7 8.6l-.3-.3V8c0-.3.1-.3.4-.4.2-.1.5-.2.7 0l.2.4v.5M4.3 8.6c.2 0 .3-.2.4-.3V8c0-.3-.2-.3-.4-.4-.2-.1-.6-.2-.8 0l-.1.4v.5" stroke="#fff" stroke-width="1.2" stroke-linecap="round"></path></svg>
-                        </Menu>
+                        <div className="right">
+                            <GetQuote to={'/get-a-quote'}>Get a quote</GetQuote>
+                            <Menu onClick={() => { setIsActive(!isActive) }}>
+                                <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M3 7h2v2H3zM7 7h2v2H7zM11 7h2v2h-2z"></path><path d="M8.6 8.8c-.5 0-1.2.1-1.4-.4 0-.2-.2-.7.1-.7.2 0 .2-.3.5-.3.1 0 .5 0 .6.2M11.7 8.6l-.3-.3V8c0-.3.1-.3.4-.4.2-.1.5-.2.7 0l.2.4v.5M4.3 8.6c.2 0 .3-.2.4-.3V8c0-.3-.2-.3-.4-.4-.2-.1-.6-.2-.8 0l-.1.4v.5" stroke="#fff" stroke-width="1.2" stroke-linecap="round"></path></svg>
+                            </Menu>
+                        </div>
                     </div>
                 </Sidebar>
             </div>
